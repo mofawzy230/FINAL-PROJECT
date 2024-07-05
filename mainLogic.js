041a89706c7841471451190d7777663e51cd951f
mofawzy230 <mofawzy230>
2024-07-05 06:06:55 +0200

    التعديل النهائى

--- mainLogic.js
+++ mainLogic.js
@@ -312,7 +312,7 @@ const navbar =
             <div class="col-9">
                 <nav class="navbar navbar-expand-lg bg-light shadow rounded pt-3">
                     <div class="container-fluid">
-                    <a class="navbar-brand" href="home.html">Tarmeez</a>
+                    <a class="navbar-brand" href="home.html">Goldsoft</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
